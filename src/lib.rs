2f627f5c24ae1719c140f860ac33e64ed14f1f45
Zac Burns <That3Percent@gmail.com>
2020-07-03 11:29:19 -0500

    Added more types; Added Firestorm

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use firestorm::profile_fn;
 use std::convert::TryInto;
 
 const BITS: [[u32; 28]; 16] = [
@@ -55,53 +56,69 @@ const COUNTS: [usize; 16] = [28, 21, 21, 21, 14, 9, 8, 7, 6, 6, 5, 5, 4, 3, 2, 1
 
 pub struct ValueOutOfRange(u32);
 
-#[inline(always)]
-fn _compress(values: &[u32]) -> Result<(u32, usize), ValueOutOfRange> {
-    for i in 0..16usize {
-        let mut value = (i as u32) << 28;
-        let count = COUNTS[i].min(values.len());
+macro_rules! impl_simple {
+    ($T:ty, $MAX:expr) => {
+        impl Simple16 for $T {
+            fn compress(values: &[Self]) -> Result<(u32, usize), ValueOutOfRange> {
+                for i in 0..($MAX as u32) {
+                    let mut value = i << 28;
+                    let count = COUNTS[i as usize].min(values.len());
 
-        let mut bits = 0;
-        let mut j = 0;
-        while j < count {
-            if values[j] >= (1 << (&BITS[i])[j]) {
-                break;
+                    let mut bits = 0;
+                    let mut j = 0;
+                    while j < count {
+                        if values[j] as u32 >= (1 << (&BITS[i as usize])[j]) {
+                            break;
+                        }
+                        value |= ((values[j] as u32) << bits);
+                        bits += (&BITS[i as usize])[j];
+                        j += 1;
+                    }
+                    if j == count {
+                        return Ok((value, j));
+                    }
+                }
+                // TODO: This is not necessarily the value that is out of range.
+                Err(ValueOutOfRange(values[0] as u32))
             }
-            value |= values[j] << bits;
-            bits += BITS[i][j];
-            j += 1;
+
+            fn consume(values: &[Self]) -> Result<usize, ValueOutOfRange> {
+                for i in 0..($MAX as usize) {
+                    let count = COUNTS[i].min(values.len());
+
+                    let mut j = 0;
+                    while j < count {
+                        if values[j] as u32 >= (1u32 << (&BITS[i])[j]) {
+                            break;
+                        }
+                        j += 1;
+                    }
+                    if j == count {
+                        return Ok(j);
+                    }
+                }
+                // TODO: This is not necessarily the value that is out of range.
+                Err(ValueOutOfRange(values[0] as u32))
+            }
         }
-        if j == count {
-            return Ok((value, j));
-        }
-    }
-    Err(ValueOutOfRange(values[0]))
+    };
 }
 
-#[inline(always)]
-fn _consume(values: &[u32]) -> Result<usize, ValueOutOfRange> {
-    for i in 0..16usize {
-        let count = COUNTS[i].min(values.len());
+// TODO: (Performance) Express that some of these are infallible
+impl_simple!(u32, 16);
+impl_simple!(u16, 16);
+impl_simple!(u8, 14);
 
-        let mut j = 0;
-        while j < count {
-            if values[j] >= (1 << (&BITS[i])[j]) {
-                break;
-            }
-            j += 1;
-        }
-        if j == count {
-            return Ok(j);
-        }
-    }
-    // TODO: This is not necessarily the value that is out of range.
-    Err(ValueOutOfRange(values[0]))
+pub trait Simple16: Sized {
+    fn consume(values: &[Self]) -> Result<usize, ValueOutOfRange>;
+    fn compress(values: &[Self]) -> Result<(u32, usize), ValueOutOfRange>;
 }
 
-pub fn calculate_size(mut values: &[u32]) -> Result<usize, ValueOutOfRange> {
+pub fn calculate_size<T: Simple16>(mut values: &[T]) -> Result<usize, ValueOutOfRange> {
+    profile_fn!(calculate_size);
     let mut size = 0;
     while values.len() > 0 {
-        let advanced = _consume(values)?;
+        let advanced = T::consume(values)?;
         values = &values[advanced..];
         size += 4;
     }
@@ -109,9 +126,10 @@ pub fn calculate_size(mut values: &[u32]) -> Result<usize, ValueOutOfRange> {
     Ok(size)
 }
 
-pub fn compress(mut values: &[u32], out: &mut Vec<u8>) -> Result<(), ValueOutOfRange> {
+pub fn compress<T: Simple16>(mut values: &[T], out: &mut Vec<u8>) -> Result<(), ValueOutOfRange> {
+    profile_fn!(compress);
     while values.len() > 0 {
-        let (next, advanced) = _compress(values)?;
+        let (next, advanced) = T::compress(values)?;
         values = &values[advanced..];
         out.extend_from_slice(&next.to_le_bytes());
     }
@@ -120,6 +138,7 @@ pub fn compress(mut values: &[u32], out: &mut Vec<u8>) -> Result<(), ValueOutOfR
 }
 
 pub fn decompress(bytes: &[u8], out: &mut Vec<u32>) -> Result<(), ()> {
+    profile_fn!(decompress);
     if bytes.len() % 4 != 0 {
         return Err(());
     }
@@ -147,8 +166,9 @@ pub fn decompress(bytes: &[u8], out: &mut Vec<u32>) -> Result<(), ()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::convert::TryFrom;
 
-    fn round_trip(data: &[u32]) {
+    fn round_trip<T: Simple16 + TryFrom<u32> + std::fmt::Debug + Eq>(data: &[T]) {
         let mut bytes = Vec::new();
         compress(&data, &mut bytes).unwrap_or_else(|_| todo!());
         assert_eq!(
@@ -157,25 +177,28 @@ mod tests {
         );
         let mut out = Vec::new();
         decompress(&bytes, &mut out).unwrap_or_else(|_| todo!());
+        let out: Vec<_> = out.into_iter().map(|o| o.try_into().unwrap_or_else(|_| panic!("round trip failed"))).collect();
 
         assert_eq!(data, &out[..data.len()]);
     }
 
     #[test]
     fn t1() {
-        let i = &[1, 5, 18, 99, 2023, 289981, 223389999];
+        let i = &[1u32, 5, 18, 99, 2023, 289981, 223389999];
+        let i = &[1u16, 5, 18, 99, 2023, u16::MAX];
+        let i = &[1u8, 5, 18, 99, u8::MAX];
         round_trip(i);
     }
 
     #[test]
     fn t2() {
-        let i = &[1];
+        let i = &[1u32];
         round_trip(i);
     }
 
     #[test]
     fn t3() {
-        let i = &[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0];
+        let i = &[0u32, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0];
         round_trip(i);
     }
 
